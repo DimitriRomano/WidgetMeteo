d00efbcbb365177f5bb0febb957eb01f47f7921f
DimitriRomano <dimitri.romano09@gmail.com>
2022-01-10 15:56:06 +0100

    little update

--- app/src/main/java/com/romano/dimitri/myweatherapp/MainActivity.java
+++ app/src/main/java/com/romano/dimitri/myweatherapp/MainActivity.java
@@ -1,10 +1,19 @@
 package com.romano.dimitri.myweatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -12,16 +21,25 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.romano.dimitri.myweatherapp.databinding.ActivityMainBinding;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private Context mContext;
-    private static final String TAG = MainActivity.class.getName();
+    private static final String TAG = "MainActivity";
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int MULTIPLE_LOCATION_REQUEST = 42;
+    private String mCityLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +50,62 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        loadWeatherData("Toulouse");
+        mCityLocation="Toulouse";
+        loadWeatherData(mCityLocation);
 
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        this.updateLocation();
 
     }
 
+    public void updateLocation() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
 
+            fusedLocationClient.getLastLocation().
+                    addOnSuccessListener(this, location -> {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Double longitude = location.getLongitude();
+                            Double latitude = location.getLatitude();
+                            Geocoder geo = new Geocoder(getApplicationContext(), Locale.getDefault());
+                            List<Address> addresses=null;
+                            try {
+                                addresses = geo.getFromLocation(latitude, longitude, 1);
+                                Log.e((TAG),addresses.get(0).toString());
+                            }catch (Exception e){
+                                Log.e(TAG,e.getMessage());
+                            }
+                            /*if(addresses!=null && addresses.size()>0){
+                                addresses.get(0).
+                            }*/
+                            Log.d(TAG,addresses.get(0).toString());
+                        }
+                    });
+        }else{
+            // Request permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET},
+                    MULTIPLE_LOCATION_REQUEST);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MULTIPLE_LOCATION_REQUEST:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                    updateLocation();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Permission denied to access device's location", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
     private void loadWeatherData(String city) {
         String url = "https://www.prevision-meteo.ch/services/json/" + city;
